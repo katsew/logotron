022aae15aa675a65335b60f4d8127e97a67f422c
katsew <y.katsew@gmail.com>
2016-09-20 02:16:29 +0900

    commands are executable

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -2,71 +2,209 @@
 
   'use strict';
 
+  const CANVAS_WIDTH = 1000;
+  const CANVAS_HEIGHT = 800;
   const canvas = <HTMLCanvasElement>document.getElementById('c');
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
   const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
 
-  class Turtle {
+  window.ctx = ctx; // for debug
 
-    private ctx: CanvasRenderingContext2D = null;
-    private posX: number = 0;
-    private posY: number = 0;
-    private queue: Array<Function> = [];
+  class Point2d {
 
-    public constructor(posX : number, posY : number, ctx: CanvasRenderingContext2D) {
-      this.posX = posX;
-      this.posY = posY;
-      this.ctx = ctx;
-    }
-
-    public run() {
-      this.ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    }
-
-    private transformAxis(x, y) {
-      return [x, y];
+    public x;
+    public y;
+    public constructor(x : number, y : number) {
+      this.x = x;
+      this.y = y;
     }
 
   }
 
-  const Commands = {
-    isDrawable: true,
-    PU: function PenUp() {
-      this.isDrawable = false;
-      console.log('--- pen up ---');
-      console.log('isDrawable: ', this.isDrawable);
-    },
-    PD: function PenDown() {
-      this.isDrawable = true;
-      console.log('--- pen down ---');
-      console.log('isDrawable: ', this.isDrawable);
-    },
-    FD: function Forward(length : number) {
-      console.log('--- forward ---');
-    },
-    BK: function Backward(length : number) {
-      console.log('--- backward ---');
-    },
-    RT: function RightTurn(degree : number) {
-      console.log('--- rotate right ---');
-      let radian = deg2rad(degree);
-      console.log(radian);
-    },
-    LT: function LeftTurn(degree : number) {
-      console.log('--- rotate left ---');
-      let radian = deg2rad(degree);
-      console.log(radian);
-    },
-    getCommandName: function getCommandName(str) {
-      return str.replace(/^(PU|PD|FD|BK|RT|LT)\((.*)\).*$/, "$1");
-    },
-    getCommandArgs: function getCommandArgs(str) {
-      return str.replace(/^(PU|PD|FD|BK|RT|LT)\((.*)\).*$/, "$2").split(',');
+  class CanvasHandler {
+
+    private ctx: CanvasRenderingContext2D = null;
+    private origin: Point2d = {
+      x: 0,
+      y: 0
+    };
+    private posX: number = 0;
+    private posY: number = 0;
+    private degree: number = 90;
+    private queue: Array<Function> = [];
+    private isDrawable: boolean = true;
+
+    public constructor(posX : number, posY : number, ctx : CanvasRenderingContext2D) {
+      this.posX = posX;
+      this.posY = posY;
+      this.ctx = ctx;
+
+      let width = this.ctx.canvas.width;
+      let height = this.ctx.canvas.height;
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+      this.origin.x = width / 2;
+      this.origin.y = height / 2;
     }
-  };
+
+    private resetCanvas() {
+      let width = this.ctx.canvas.width;
+      let height = this.ctx.canvas.height;
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+
+    public run() {
+
+      this.resetCanvas();
+
+      this.posX = this.origin.x;
+      this.posY = this.origin.y;
+      this.ctx.moveTo(this.posX, this.posY);
+
+      this.queue.forEach(function(command) {
+        command();
+      });
+
+      console.log('--- finish exec command, stroke it ---');
+      this.ctx.stroke();
+    }
+
+    private transformX(x : number) : number {
+      return this.posX + x;
+    }
+
+    private transformY(y : number) : number {
+      return this.posY + y;
+    }
+
+    public enqueue(func) {
+      this.queue.push(func);
+    }
+
+    public setDrawableTo(isDrawable : boolean) : void {
+      this.isDrawable = isDrawable;
+      console.log('set isDrawable to : ', this.isDrawable);
+    }
+
+    public moveForward(length : number) {
+
+      let radian = deg2rad(this.degree);
+      this.posX = this.transformX(Math.cos(radian) * length);
+      this.posY = this.transformY(Math.sin(radian) * length);
+
+      if (this.isDrawable) {
+        this.ctx.lineTo(this.posX, this.posY);
+      } else {
+        this.ctx.moveTo(this.posX, this.posY);
+      }
+
+      console.log('move to :', this.posX, this.posY);
+
+    }
+
+    public moveBackward(length : number) {
+      this.moveForward(-length);
+    }
+
+    public rotateRight(degree : number) {
+      this.degree = this.degree - degree;
+      console.log('rotate right: ', this.degree);
+    }
+
+    public rotateLeft(degree : number) {
+      this.degree = this.degree + degree;
+      console.log('rotate left: ', this.degree);
+    }
+
+  }
+
+  const INPUT_PARSE_REGEXP = /^(PU|PD|FD|BK|RT|LT)\((.*)\).*$/;
+  class InputHandler {
+
+    private handler: CanvasHandler;
+
+    public constructor(c : CanvasHandler) {
+      this.handler = c;
+    }
+    public PU() {
+      console.log('--- input command: pen up ---');
+      this.handler.enqueue(this.handler.setDrawableTo.bind(this.handler, false));
+    }
+    public PD() {
+      console.log('--- input command: pen down ---');
+      this.handler.enqueue(this.handler.setDrawableTo.bind(this.handler, true));
+    }
+    public FD(length : number) {
+      console.log('--- input command: forward ---');
+      this.handler.enqueue(this.handler.moveForward.bind(this.handler, length));
+    }
+    public BK(length : number) {
+      console.log('--- input command: backward ---');
+      this.handler.enqueue(this.handler.moveBackward.bind(this.handler, length));
+    }
+    public RT(degree : number) {
+      console.log('--- input command: rotate right ---');
+      this.handler.enqueue(this.handler.rotateRight.bind(this.handler, degree));
+    }
+    public LT(degree : number) {
+      console.log('--- input command: rotate left ---');
+      this.handler.enqueue(this.handler.rotateLeft.bind(this.handler, degree));
+    }
+    public static getCommandName(str) {
+      return str.replace(INPUT_PARSE_REGEXP, "$1");
+    }
+    public static getCommandArgs(str) {
+      return str.replace(INPUT_PARSE_REGEXP, "$2").split(',');
+    }
+  }
+
+  const textarea = <HTMLTextAreaElement>document.getElementById('editCommand');
+  const button = <HTMLButtonElement>document.getElementById('buttonRun');
+
+  button.addEventListener('click', function(e) {
+
+    const canvasHandler = new CanvasHandler(canvas.width/2, canvas.height/2, ctx);
+    const inputHandler = new InputHandler(canvasHandler);
+
+    const inputs = textarea.value;
+    const commands = parseCommands(inputs);
+    console.log(commands);
+    commands.filter(function(command) {
+      return command !== '' && command != null;
+    }).forEach(function (command) {
+      let commandName = InputHandler.getCommandName(command);
+      let commandArgs = InputHandler.getCommandArgs(command);
+      try {
+        inputHandler[commandName].apply(inputHandler, commandArgs);
+        console.log(commandName);
+        console.log(commandArgs);
+      } catch (e) {
+        console.warn(e.name);
+        console.warn(e.message);
+        console.warn(commandName);
+        console.warn(commandArgs);
+      }
+    });
+
+    console.log('--- finish enqueue commands ---');
+    canvasHandler.run();
+
+  }, false);
+
+  function parseCommands(commands : string) : Array<string> {
+
+    if (commands === '' || commands.length < 1) return [];
+    const parsed = commands.split(/\r\n|\r|\n/);
+    if (Array.isArray(parsed)) {
+      return parsed;
+    }
+    return [];
+
+  }
 
   function deg2rad(degree : number) : number {
 
-    // 半径を1としたときの直径は2でその円周の長さは2 * Math.PI
+    // 半径を1としたときの直径は2でその円周の長さは2 * 1 * Math.PI
     // したがって、弧度法で測った場合の角度degreeは degree / 360 * 2 * 1 * Math.PI
     let radian = degree / 180 * Math.PI;
     console.log(radian);
@@ -74,48 +212,4 @@
 
   }
 
-  const textarea = <HTMLTextAreaElement>document.getElementById('editCommand');
-  const button = <HTMLButtonElement>document.getElementById('buttonRun');
-  button.addEventListener('click', function(e) {
-    const inputs = textarea.value;
-    const commands = parseCommands(inputs);
-    console.log(commands);
-    let width = canvas.width;
-    let height = canvas.height;
-    ctx.moveTo(width / 2, height / 2);
-    commands.filter(function(command) {
-      return command !== '' && command != null;
-    }).forEach(function (command) {
-      let commandName = Commands.getCommandName(command);
-      console.log(commandName);
-      let commandArgs = Commands.getCommandArgs(command);
-      console.log(commandArgs);
-      try {
-        Commands[commandName].apply(Commands, commandArgs);
-      } catch (e) {
-        console.warn(e.message);
-      }
-    });
-
-    console.log('--- finish calling commands ---');
-    ctx.stroke();
-
-  }, false);
-
-  function parseCommands(commands : string) : Array<string> {
-    if (commands === '' || commands.length < 1) return [];
-    const parsed = commands.split(/\r\n|\r|\n/);
-    if (Array.isArray(parsed)) {
-      return parsed;
-    }
-    return [];
-  }
-
-  let cancelId : number;
-  function main () {
-    cancelId = window.requestAnimationFrame(main);
-
-  }
-  main();
-  console.log(cancelId);
 })();
